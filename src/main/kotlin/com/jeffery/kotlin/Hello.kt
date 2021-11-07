f03b9039546ae980a37451cb97d708b61a6533e1
Jeffery0001 <chen.a905420@gmail.com>
2021-11-07 15:08:08 +0800

    Initial commit

---
+++ src/main/kotlin/com/jeffery/kotlin/Hello.kt
@@ -0,0 +1,15 @@
+package com.jeffery.kotlin
+
+fun main() {
+//    println("Hello Kotlin")
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+
+}
